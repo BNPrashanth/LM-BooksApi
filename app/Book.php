9ed968573f5b389908903b8eaecbafbdd4c94eef
BNPrashanth <bnprashanth256@gmail.com>
2019-05-12 18:11:24 +0530

    migration and model created for Books

---
+++ app/Book.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Book extends Model
+{
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'title',
+        'description',
+        'price',
+        'author_id',
+    ];
+
+    /**
+     * The attributes excluded from the model's JSON form.
+     *
+     * @var array
+     */
+    protected $hidden = [
+        'password',
+    ];
+}
